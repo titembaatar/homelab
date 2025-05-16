f392aaab89a30c1e4520d6ca19b0223d4e86379e
titembaatar <etienne.guillemin@protonmail.com>
2025-05-16 15:20:03 +0200

    gateway stack internal network

--- scripts/docker/deploy/gateway.sh
+++ scripts/docker/deploy/gateway.sh
@@ -4,5 +4,6 @@ set -e
 "$HOME"/homelab/scripts/docker/debian-install.sh
 "$HOME"/homelab/scripts/docker/manager.sh
 
+docket network create gateway_net
 docker compose -f "$HOME"/homelab/docker/caddy/docker-compose.yaml up -d
 docker compose -f "$HOME"/homelab/docker/gateway/docker-compose.yaml up -d
